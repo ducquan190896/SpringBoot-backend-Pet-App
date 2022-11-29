b19025a2fd4c220b5b7032486e29f192acc2658a
quan doan <ducquan190896@gmail.com>
2022-11-29 19:16:39 +0200

    third

--- src/main/java/com/quan/petcaringapp/Service/CustomerService.java
+++ src/main/java/com/quan/petcaringapp/Service/CustomerService.java
@@ -19,6 +19,12 @@ public class CustomerService {
     }
     
     public List<Customer> getCustomers1() {
+
+        // em in cái data trong terminal lấy từ database thì vẫn bình thường, nhừng send nó bằng postman thì lại bị lỗi
+        customerRepos.findAll().stream().forEach(cust -> System.out.println(cust));
+
+        //cái này là em test thử bằng cách tạo một cái list rồi add customer manually, cách này thì nó lại chạy đc trên postman.
+        
         List<Customer> list = new ArrayList<>();
         Customer customer1 = new Customer("Thoa", "0412456789", "Thoa@gmail.com", "my pet is sick, take it to the hospital");
         list.add(customer1);
